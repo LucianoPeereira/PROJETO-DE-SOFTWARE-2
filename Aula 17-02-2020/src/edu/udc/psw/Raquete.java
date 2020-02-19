59fe21968c714fbb2f45c6e4ee9b788a9eb7567e
LucianoPeereira <marcolucianopereira@hotmail.com>
2020-02-18 21:23:39 -0300

    Exercicio da aula 17/02 - Corrigido

--- Aula 17-02-2020/src/edu/udc/psw/Raquete.java
+++ Aula 17-02-2020/src/edu/udc/psw/Raquete.java
@@ -3,42 +3,41 @@ package edu.udc.psw;
 import java.io.Serializable;
 
 public class Raquete implements Serializable{
-	
-	float peso ;
-	String cor;
+	float peso;
 	int comprimento;
-			
-	public Raquete(float peso, String cor, int comprimento) {
-		super();
+	String cor;
+	
+	public Raquete(float peso, int comprimento, String cor) {
 		this.peso = peso;
-		this.cor = cor;
 		this.comprimento = comprimento;
+		this.cor = cor;	
 	}
 	
 	public float getPeso() {
 		return peso;
 	}
+
 	public void setPeso(float peso) {
 		this.peso = peso;
 	}
-	public String getCor() {
-		return cor;
-	}
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
+
 	public int getComprimento() {
 		return comprimento;
 	}
+
 	public void setComprimento(int comprimento) {
 		this.comprimento = comprimento;
 	}
-	@Override
-	public String toString() {
-		return "Raquete [peso=" + peso + ", cor=" + cor + ", comprimento=" + comprimento + "]";
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
 	}
 	
-	
-	
-
+	public String toString() {
+		return comprimento + "/" + cor;
+	}
 }
